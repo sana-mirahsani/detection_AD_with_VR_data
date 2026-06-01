6bd8a68bc23dd4be1f637d492cd759710b12bc10
sana <s.mirahsani1998@gmail.com>
2026-06-01 19:27:29 +0200

    extract feature vector section 1

--- src/features/data_pipeline.py
+++ src/features/data_pipeline.py
@@ -1,5 +1,9 @@
 from features import data_utils
 from pathlib import Path
+import pandas as pd
+import sys
+sys.path.append('../') 
+from features import data_utils as du
 
 def pipeline_cleaning(list_patient_id: list,
                       patients_data_folder: str) -> None:
@@ -36,3 +40,93 @@ def pipeline_cleaning(list_patient_id: list,
             
         print("Cleaning OK, next patient...")
     print("Cleaning process completed.")
+
+def creating_feature_vector_section_default(section_default_df: pd.DataFrame,
+                                            hover_dict: dict,
+                                            press_dict: dict,
+                                            reading_time_dict: dict,
+                                            behavior_dict: dict,
+                                            temporal_dict: dict) -> pd.DataFrame: 
+
+    features_dict = {
+        "section_id": None,
+        "section_name": None,
+        "section_total_time": None,
+    }
+    
+    # extract section duration
+    row_section_end = section_default_df[section_default_df['EventType']=='SECTION_END']['Activity_Log'].values[0]
+    section_total_time = du.extract_section_duration(row_section_end)
+
+    # fill out hover_dict
+    # hover count
+    filtered_df = du.filter_by_string_contains(section_default_df, 'Activity_Log', 'HoverCount')
+    hover_count_series = du.extract_metric_from_section(filtered_df, du.extract_hover_count)
+    hover_dict["total_hover_count"], _ , _ , _ , _ = du.calculate_metric_stats(hover_count_series, is_counting=True)
+
+    # hover duration
+    filtered_df = du.filter_by_string_contains(section_default_df, 'Activity_Log', 'HoverDuration')
+    hover_duration_series = du.extract_metric_from_section(filtered_df, du.extract_hover_duration)
+    hover_dict["total_hover_duration"], hover_dict["mean_hover_duration"], hover_dict["max_hover_duration"], hover_dict["median_hover_duration"], hover_dict["std_hover_duration"] = du.calculate_metric_stats(hover_duration_series, is_counting=False)
+
+    # hover intensity
+    hover_dict["hover_intensity"] = du.ratio_calculation(hover_dict["total_hover_duration"], section_total_time)
+
+    # hover cv (Coefficient of Variation)
+    hover_dict["cv_hover_duration"] = du.ratio_calculation(hover_dict["std_hover_duration"], hover_dict["mean_hover_duration"])
+
+    # fill out press_dict
+    # press count (assumed that for each button pressed, there is a button released too
+    filtered_df_button_pressed = du.filter_by_string_contains(section_default_df, 'EventType', 'BUTTON_PRESSED')
+    total_press_count = len(filtered_df_button_pressed)  # Assuming each press has a corresponding release
+    press_dict["total_press_count"] = total_press_count
+
+    # press duration
+    filtered_df = du.filter_by_string_contains(section_default_df, 'Activity_Log', 'PressDuration')
+    press_duration_series = du.extract_metric_from_section(filtered_df, du.extract_press_duration)
+
+    press_dict["total_press_duration"], press_dict["mean_press_duration"], press_dict["max_press_duration"], press_dict["median_press_duration"], press_dict["std_press_duration"] = du.calculate_metric_stats(press_duration_series, is_counting=False)
+
+    # press_intensity
+    press_dict["press_intensity"] = du.ratio_calculation(press_dict["total_press_duration"], section_total_time)
+
+    # fill out reading_time_dict
+    # reading time duration
+    filtered_df = du.filter_by_string_contains(section_default_df, 'Activity_Log', 'ReadingTime')
+    reading_time_series = du.extract_metric_from_section(filtered_df, du.extract_reading_time_duration)
+
+    # calculation for reading time duration
+    reading_time_dict["total_reading_time_duration"], reading_time_dict["mean_reading_time_duration"], reading_time_dict["max_reading_time_duration"], reading_time_dict["median_reading_time_duration"], reading_time_dict["std_reading_time_duration"] = du.calculate_metric_stats(reading_time_series, is_counting=False)
+
+    # reading time intensity
+    reading_time_dict["reading_time_intensity"] = du.ratio_calculation(reading_time_dict["total_reading_time_duration"], section_total_time)
+
+    # fill out behavior_dict
+    behavior_dict["hover_vs_active_interaction_ratio"] = du.ratio_calculation(hover_dict["total_hover_duration"], press_dict["total_press_duration"])
+    behavior_dict["hover_vs_reading_time_ratio"] = du.ratio_calculation(hover_dict["total_hover_duration"], reading_time_dict["total_reading_time_duration"])
+
+    behavior_dict["interaction_fraction"] = du.ratio_calculation(hover_dict["total_hover_duration"] + press_dict["total_press_duration"], section_total_time)
+
+    behavior_dict["decision_latency"] = du.calculate_decision_latency(first_hover_time=du.extract_first_time_hover(section_default_df), first_press_time=du.extract_first_time_press(section_default_df))
+
+    behavior_dict["clicks_per_second"] = du.ratio_calculation(press_dict["total_press_count"], section_total_time)
+    behavior_dict["hovers_per_click"] = du.ratio_calculation(hover_dict["total_hover_count"], press_dict["total_press_count"])
+
+    # fill out temproal_dict
+    temporal_dict['time_before_first_press'] = du.extract_first_time_press(section_default_df)
+    temporal_dict['time_before_first_hover'] = du.extract_first_time_hover(section_default_df)
+
+    # create dataframe
+    features_dict["section_id"] = 1
+    features_dict["section_name"] = 'Default'
+    features_dict["section_total_time"] = section_total_time
+
+    features_dict.update(hover_dict)
+    features_dict.update(press_dict)
+    features_dict.update(reading_time_dict)
+    features_dict.update(behavior_dict)
+    features_dict.update(temporal_dict)
+
+    return pd.DataFrame([features_dict])
+
+def creating_feature_vector_tutorial_events():pass
--- src/features/data_utils.py
+++ src/features/data_utils.py
@@ -1,5 +1,6 @@
 import re
 import pandas as pd
+from datetime import datetime
 
 # Find the paitent folder in the data folder
 def find_patient_folder(patients_data_folder: str, patient_id: str) -> dir:
@@ -76,13 +77,180 @@ def save_df(df, csv_path):
     except Exception as e:
         return False, str(e)
 
-# feature extraction of tutorial events
-def extract_section(df: pd.DataFrame, section_name: str) -> pd.DataFrame:
+# Filtering functions ========================================================
+def filter_on_section(df: pd.DataFrame, section_name: str) -> pd.DataFrame:
+    """
+    Extracts rows from the DataFrame that belong to a specific section.
+    Args:        
+        df (pd.DataFrame): The input DataFrame containing the data.
+        section_name (str): The name of the section to extract.
+    Returns:        
+        pd.DataFrame: A DataFrame containing only the rows that belong to the specified section.
+    """
     df_with_one_section = df[df['Section'] == section_name]
     return df_with_one_section
 
-def extract_event_types(df : pd.DataFrame, events_to_ignore: list) -> list:
-    event_types = df['EventType'].unique()
-    return [event for event in event_types if event not in events_to_ignore]
+def filter_by_string_contains(df: pd.DataFrame, column_name: str, substring: str) -> pd.DataFrame:
+    """
+    Filters the DataFrame to include only rows where the specified column contains a given substring.
+    Args:
+        df (pd.DataFrame): The input DataFrame to filter.
+        column_name (str): The name of the column to check for the substring.
+        substring (str): The substring to look for within the specified column.
+    Returns:
+        pd.DataFrame: A DataFrame containing only the rows where the specified column contains the given substring.
+    """
+    return df[df[column_name].str.contains(substring, na=False)]
 
-#def extract_dict_hover_button
+# Extraction functions =========================================================
+def extract_section_duration(s: str)-> float:
+    """
+    Extracts the duration of a section from Activity log of event_type == section_end.
+    Args:       
+        s (str): A string that contains the section end information.
+    Returns:        
+        float: The duration of the section in seconds, or None if the duration cannot be extracted
+    """
+    match = re.search(r"Duration=([0-9]*\.?[0-9]+)", s)
+    return float(match.group(1)) if match else None
+
+def extract_hover_count(s: str)-> int:
+    """
+    Extracts the hover count from a string of Activity_Log that contains the hover information.
+    Args:       
+        s (str): Value of Activity_Log columns that contains the hover information of HoverCount=
+    Returns:
+        int: The hover count, or None if the hover count cannot be extracted"""
+    
+    match = re.search(r"HoverCount=(\d+)", s)
+    return int(match.group(1)) if match else None
+
+def extract_hover_duration(s: str)-> float:
+    """
+    Extracts the hover duration from a string of Activity_Log that contains the hover information.
+    Args:       
+        s (str): Value of Activity_Log columns that contains the hover information of HoverDuration=
+    Returns:
+        float: The hover duration, or None if the hover duration cannot be extracted."""
+    
+    match = re.search(r"HoverDuration=([0-9]*\.?[0-9]+)", s)
+    return float(match.group(1)) if match else None
+
+def extract_press_duration(s: str)-> float:
+    """
+    Extracts the press duration from a string of Activity_Log that contains the press information.
+    Args:
+        s (str): Value of Activity_Log columns that contains the press information of PressDuration=
+    Returns:
+        float: The press duration, or None if the press duration cannot be extracted."""
+    
+    match = re.search(r"PressDuration=([0-9]*\.?[0-9]+)", s)
+    return float(match.group(1)) if match else None
+
+def extract_reading_time_duration(s: str)-> float:
+    """
+    Extracts the reading time duration from a string of Activity_Log that contains the reading time information.
+    Args:
+        s (str): Value of Activity_Log columns that contains the reading time information of ReadingTime=
+    Returns:
+        float: The reading time duration, or None if the reading time duration cannot be extracted."""
+    
+    match = re.search(r"ReadingTime=([0-9]*\.?[0-9]+)", s)
+    return float(match.group(1)) if match else None
+
+def extract_metric_from_section(filtered_df: pd.DataFrame, function_to_extract_metric: callable) -> pd.Series:
+    """
+    Extracts a specific metric from the 'Activity_Log' column of a filtered DataFrame using a provided extraction function.
+    Args:
+        filtered_df (pd.DataFrame): A DataFrame that has been filtered to contain only the relevant section.
+        function_to_extract_metric (callable): A function that takes a string as input and extracts the desired metric from it.
+    Returns:
+        pd.Series: A pandas Series containing the extracted metric values for each row in the filtered Data
+    """
+    section_metric_values = []
+    for value in filtered_df['Activity_Log']:
+        #print(function_to_extract_metric(value))
+        section_metric_values.append(function_to_extract_metric(value))
+
+    section_metric_values_pandas = pd.Series(section_metric_values)
+    return section_metric_values_pandas
+
+def extract_first_time_hover(section_df: pd.DataFrame) -> float:
+    """
+    Extracts the time of the first hover event from a section DataFrame.
+    Args:   
+        section_df (pd.DataFrame): A DataFrame containing the data for a specific section, which should include 'EventType' and 'PhaseTime_s' columns.
+    Returns:
+        pd.Series: A pandas Series containing the time of the first hover event in seconds, or
+    """
+    first_hover_time = section_df[section_df['EventType'] == 'BUTTON_HOVER_START'].iloc[0]['PhaseTime_s']
+    return first_hover_time
+
+def extract_first_time_press(section_df: pd.DataFrame) -> float:
+    """
+    Extracts the time of the first click event from a section DataFrame.
+    Args:   
+        section_df (pd.DataFrame): A DataFrame containing the data for a specific section, which should include 'EventType' and 'PhaseTime_s' columns.
+    Returns:
+        pd.Series: A pandas Series containing the time of the first click event in seconds, or None if there are no click events.
+    """
+    first_click_time = section_df[section_df['EventType'] == 'BUTTON_PRESSED'].iloc[0]['PhaseTime_s']
+    return first_click_time
+
+# Calculation functions =========================================================
+def calculate_metric_stats(metric_series: pd.Series, is_counting: bool=True) -> tuple:
+    """
+    Calculates statistics for a given metric series.
+    Args:
+        metric_series (pd.Series): The series of metric values.
+        is_counting (bool): Whether the metric is a count (True) or duration (False).
+    Returns:
+        tuple: A tuple containing the calculated statistics.
+    """
+    # calculate total sum for both counting and duration metrics
+    total = round(metric_series.sum(),2)
+    #print("Sum:", total)
+
+    # For counting metrics, we only care about the total sum.
+    if is_counting:
+        total = metric_series.sum()
+        return total, None, None, None, None
+    
+    # For duration metrics, we calculate all statistics.
+    mean = round(metric_series.mean(), 2)
+    maximum = round(metric_series.max(), 2)
+    median = round(metric_series.median(), 2)
+    std = round(metric_series.std(), 2)
+
+    """
+    print("Mean:", mean)
+    print("Max:", maximum)
+    print("Median:", median)
+    print("Std:", std)
+    """
+    return total, mean, maximum, median, std
+
+def ratio_calculation(value1: float, value2: float) -> float:
+    """
+    Calculates the ratio of two values.
+    Args:
+        value1 (float): The numerator.
+        value2 (float): The denominator.
+    Returns:
+        float: The calculated ratio, or None if the denominator is zero.
+    """
+    if value2 == 0:
+        return None  # or you could return float('inf') to indicate an infinite ratio
+    return round(value1 / value2, 2)
+
+def calculate_decision_latency(first_hover_time: float, first_press_time: float) -> float:
+    """
+    Calculates the decision latency, which is the time difference in seconds between the first hover and the first press in a given section DataFrame.
+    Args:
+        first_hover_time (float): The time of the first hover event in seconds.
+        first_press_time (float): The time of the first press event in seconds.
+    Returns:
+        float: The decision latency in seconds, or None if the timestamps cannot be parsed.
+    """
+    time_difference = first_press_time - first_hover_time
+    return time_difference
